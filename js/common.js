fe0ccc1f7ea0c0b00087f63c5321ce92ba7d28a7
KarenChuan <lanyonyon@gmail.com>
2023-01-30 16:08:53 +0800

    1.新增index.js 2.common.js修改 3.每頁連結common.js

--- js/common.js
+++ js/common.js
@@ -1,35 +1,16 @@
-let mySwiper = new Swiper(".swiper", {
-  effect: "fade",
-  adeEffect: {
-    crossFade: true,
-  },
-  speed: 1000,
-  autoplay: true,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    bulletElement: "li",
-    bulletClass: "banner-dot",
-    bulletActiveClass: "dot-active",
-  },
-});
 
-AOS.init();
 
-let btn = document.querySelectorAll('.read_more')[0];
-let popUp = document.getElementsByClassName('pop_up_overlay')[0];
-let popClose = document.getElementsByClassName('svg_close')[0];
-btn.addEventListener('click', function () { 
-    if (popUp.style.display=='none') { 
-        popUp.style.display = '';
-    } 
-});
-popClose.addEventListener('click', function () {
-    if (popUp.style.display!='none') { 
-        popUp.style.display = 'none';
-    } 
-});
-
+// nav scroll
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementsByTagName('nav')[0].style.top = "0";
+  } else {
+    document.getElementsByTagName("nav")[0].style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
 
 
 
