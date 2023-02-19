668966174667fa16445f5a2dd8483c3bc55c77e1
KarenChuan <lanyonyon@gmail.com>
2023-02-20 04:03:09 +0800

    雷到自己更新回最新檔案

--- js/common.js
+++ js/common.js
@@ -1,16 +1,37 @@
-AOS.init();
+ocument.addEventListener("DOMContentLoaded", function () {
 
-// nav scroll
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementsByTagName('nav')[0].style.top = "0";
-  } else {
-    document.getElementsByTagName("nav")[0].style.top = "-100px";
+  let screenWidth = document.body.clientWidth;
+  let hamburger = document.querySelector('input.trigger');
+  let menu = document.querySelector('ul.menu');
+
+  function toggleHamburger() {
+    if (hamburger.checked) {
+      menu.style.display = '';
+    } else {
+      menu.style.display = 'none';
+    }
   }
-  prevScrollpos = currentScrollPos;
-}
+
+  if (screenWidth <= 414) {
+    menu.style.display = 'none';
+    window.addEventListener('click', function () {
+      toggleHamburger();
+    });
+  } else {
+    menu.style.display = '';
+    // nav scroll
+    let prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementsByTagName('nav')[0].style.top = "0";
+      } else {
+        document.getElementsByTagName("nav")[0].style.top = "-100px";
+      }
+      prevScrollpos = currentScrollPos;
+    }
+  }
+});
 
 
 
--- js/gallery.js
+++ js/gallery.js
@@ -1,24 +1,19 @@
 //Isotope filter Menu
-// init Isotope
-var $grid = $(".pics_area").isotope({
-  itemSelector: "li",
-  layoutMode: "fitRows",
-  // transitionDuration: 400,
-  // stagger: '0.1s',
+$('.pics_area').imagesLoaded(function () {
+  // init Isotope
+  var $grid = $(".pics_area").isotope({
+    itemSelector: "li",
+    masonry: {
+      columnWidth: 355,
+      horizontalOrder: true,
+    },
+  });
 });
-
 // bind filter button click
 $("#filter_btn").on("click", "li", function () {
   var filterValue = $(this).attr("data-filter");
   $grid.isotope({
-    // filter: function () {
-    //   if (filterValue !== "*") {
-    //     console.log('5555');
-    //     filterValue = "." + filterValue;
-    //   }
-    //   return filterValue;
-    // }
-    filter: "." + filterValue,
+    filter: filterValue,
   });
 });
 
@@ -31,11 +26,26 @@ $("#filter_btn").each(function (i, filterBtn) {
   });
 });
 
-// infinitePlug
-//   let elem = document.querySelector('.pics_area');
-//   let infScroll = new InfiniteScroll(elem, {
-//     // options
-//     //path: '.pagination__next',
-//     append: 'li',
-//     history: false,
-//   });
+//------------ Collect favorite ----------------//
+const heart = document.getElementsByClassName("fa-heart");
+const span = document.getElementsByClassName("count");
+for (let i = 0; i < heart.length; i++) {
+  let count;//計數器
+  (function (i) {
+    // 點選最愛
+    let flag = true;
+    heart[i].onclick = function () {
+      if (flag) {
+        flag = false;
+        // 最愛點選後變紅
+        this.className = "fa-solid fa-heart active";
+        ++span[i].innerHTML;  //在最愛+1
+      } else { //取消最愛
+        flag = true;
+        this.className = "fa-solid fa-heart";
+        --span[i].innerHTML; //取消最愛-1
+      }
+    }
+  })(i);
+}
+
